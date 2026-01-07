eb2b4fa426f9e415da506d8985756f90ed578768
Piotr_Szpąt <151458713+Pietruszkaa@users.noreply.github.com>
2026-01-07 21:11:48 +0100

    Initial commit (V0.1)

---
+++ settings.py
@@ -0,0 +1,37 @@
+BOT_VERSION = "0.1.0"
+
+# ==== Channels ==== #
+WELCOME_CHANNEL_ID = 888451506402127973
+GOODBYE_CHANNEL_ID = 888552715922341969
+MC_STATUS_CHANNEL_ID = 1457828765223751762
+MC_STATUS_MESSAGE_ID = 1457832119467380854
+MEMBER_COUNT_CHANNEL_ID = 1458231426699755694
+
+# ==== Minecraft ==== #
+MC_ADDRESS = "szpatowice.playit.game"
+MC_PORT = 25565
+MC_UPDATE_INTERVAL = 10
+
+# ==== Welcome texts ==== #
+WELCOME_TEXTS = [
+    "🔥 Wszedł jak do siebie",
+    "😈 Kolejna dusza do kolekcji",
+    "🗿 Odważny ruch",
+    "💀 Nie ma odwrotu",
+]
+
+# ==== Goodbye texts ==== #
+GOODBYE_TEXTS = [
+    "🕯️ Pamiętajmy go takim, jakim był",
+    "🪦 R.I.P. połączenie z serwerem",
+    "🚪 Wyszedł. Drzwi się nie zamknęły.",
+]
+
+# ==== Embed colors ==== #
+WELCOME_COLOR = 0x00ff00
+GOODBYE_COLOR = 0xff0000
+
+# ==== Features ==== #
+ENABLE_WELCOME = True
+ENABLE_GOODBYE = True
+ENABLE_MC_STATUS = True
